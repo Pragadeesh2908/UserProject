a5532fb5118339ceee96ff2f2cc566420c8b2f62
pragadeesh <pragadeesh1100@gmail.com>
2024-10-09 13:16:12 +0530

    bug

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -25,7 +25,6 @@ class PasswordController extends Controller
             'new_password' => 'required|string|min:8|confirmed',
         ]);
 
-        // Check if the current password is correct
         if (!Hash::check($request->current_password, Auth::user()->password)) {
             return redirect()->back()->withErrors(['current_password' => 'Current password is incorrect.']);
         }
@@ -53,7 +52,6 @@ class PasswordController extends Controller
 
         $email_exists = DB::table('password_resets')->where('email', $email)->exists();
         $expire_time= DB::table('password_resets')->where('email', $email)->value('expires_at');
-        // dd($expire_time);
         if(!$email_exists || ($email_exists && Carbon::now()->greaterThan($expire_time))){
             DB::table('password_resets')->insert([
                 'email' => $request->input('email'),
@@ -66,9 +64,6 @@ class PasswordController extends Controller
             return back()->with('info', 'The reset password link is already sent.');
         }
         $user = User::where('email', $request->input('email'))->first();
-        if($user){
-            
-        }
         Mail::send('email.forgotPassword', ['token' => $token, 'user' => $user], function ($message) use ($request) {
             $message->to($request->input('email'));
             $message->subject('Reset Password');
